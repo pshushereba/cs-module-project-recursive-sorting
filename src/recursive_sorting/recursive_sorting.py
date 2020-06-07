945a5c1024bdd49857e762c3a8a2ce6cf9524e39
Patrick Shushereba <pshushereba@gmail.com>
2020-06-06 17:01:43 -0700

    Finished MVP.

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,4 +1,3 @@
-# TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
@@ -6,6 +5,7 @@ def merge(arrA, arrB):
     j = 0
     k = 0
     # Your code here
+
     while i < len(arrA) and j < len(arrB):
         if arrA[i] < arrB[j]:
             merged_arr[k] = arrA[i]
@@ -31,31 +31,65 @@ def merge(arrA, arrB):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort(arr):
     # Your code here
-    if len(arr) == 1:
-        return
-    
+    if len(arr) <= 1:
+        return arr
+
     middle = len(arr) // 2
     left_arr = arr[:middle]
     right_arr = arr[middle:]
+    left_arr = merge_sort(left_arr)
+    right_arr = merge_sort(right_arr)
 
-    merge(left_arr, right_arr)
+    arr = merge(left_arr, right_arr)
 
     return arr
-
-print(merge_sort([5,8,12,1,14,7,15]))
 
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    left_start = start
+    left_end = mid
+    right_start = mid + 1
+    right_end = end
 
+    while left_start <= left_end and right_start <= right_end:
+        if arr[left_start] <= arr[right_start]:
+            left_start += 1
+        else:
+            temp = arr[right_start]
+            index = right_start
+            while index > left_start:
+                arr[index] = arr[index - 1]
+                index -= 1
+            arr[left_start] = temp
+            left_start += 1
+            left_end += 1
+            right_start += 1
 
     return arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
+    mid = l + (r - l) // 2
+    
+    if len(arr) <= 1:
+        return arr
+
+    if r == l:
+        return arr
+    
+    
+    left_start = l
+    left_end = mid
+    right_start = mid + 1
+    right_end = r
 
 
+    merge_sort_in_place(arr, left_start, left_end)
+    merge_sort_in_place(arr, right_start, right_end)
+
+    merge_in_place(arr, left_start, left_end, right_end)
     return arr
 
 
